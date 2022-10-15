403b06d54489403e77cea3de5a9c34b12d97e8f3
dandgreat <dandivth@gmail.com>
2022-10-15 15:49:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 
 /**
 * main - prints exactly "and that piece of art is useful"
-* - Dora Korpar, 2015-10-19,
-*   followed by a new line to a standard error.
-*   Return: always 0 (Success)
+* - Dora Korpar, 2015-10-19",
+* followed by a new line, to a standard error.
+* Return: always 0 (Success)
 */
 int main(void)
 {
